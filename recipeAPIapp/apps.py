7f5d299b895bb36a7c10c2b46550828723658fba
rezvoj <rezac.vojtech.0@gmail.com>
2022-12-19 14:14:55 +0100

    content limits

--- recipeAPIapp/apps.py
+++ recipeAPIapp/apps.py
@@ -7,3 +7,18 @@ class Config(AppConfig):
     class IssueFor:
         jwt_token = 7
         email_code = 3
+
+    class PerRecipeLimits:
+        categories = 10
+        photos = 10
+        instructions = 15
+        ingredients = 20
+
+    class ContentLimits:
+        """ (pieces, per hours) """
+        inventory_limit = 50
+        email_code = (7, 1)
+        recipe = (5, 24)
+        recipe_moderator = (20, 24)
+        rating = (15, 24)
+        report = (15, 24)

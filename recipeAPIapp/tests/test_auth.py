4ad5819345a327dbd6d715066cc1e04ec5f14a53
rezvoj <rezac.vojtech.0@gmail.com>
2023-01-07 22:03:34 +0100

    auth login and update tests

--- recipeAPIapp/tests/test_auth.py
+++ recipeAPIapp/tests/test_auth.py
@@ -31,3 +31,133 @@ class TestToken(APITestCase):
         response: Response = self.client.post('/auth/token', **headers, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertNotIn('token', response.data)
+
+
+class TestLogin(APITestCase):    
+    def setUp(self):
+        self.user = User(email="test@example.com", name="Test User")
+        security.set_password(self.user, "testpassword")
+        self.user.save()
+
+    def test_valid_credentials(self):
+        response: Response = self.client.post(
+            '/auth/login', data={
+                "email": "test@example.com",
+                "password": "testpassword"
+            }, format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+
+    def test_invalid_email(self):
+        response: Response = self.client.post(
+            '/auth/login', data={
+                "email": "wrongemail@example.com",
+                "password": "testpassword"
+            }, format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        expected_data = {'detail': {'non_field_errors': ['invalid email or password.']}}
+        self.assertEqual(response.data, expected_data)
+
+    def test_login_invalid_password(self):
+        response: Response = self.client.post(
+            '/auth/login', data={
+                "email": "test@example.com",
+                "password": "wrongpassword"
+            }, format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        expected_data = {'detail': {'non_field_errors': ['invalid email or password.']}}
+        self.assertEqual(response.data, expected_data)
+
+
+@override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend')
+class TestUpdate(APITestCase):    
+    def setUp(self):
+        self.user = User(email="test@example.com", name="Test User")
+        security.set_password(self.user, "testpassword")
+        self.user.save()
+        self.token = security.generate_token(self.user)
+    
+    def test_update_email(self):
+        new_email = "newemail@example.com"
+        headers = {'HTTP_AUTHORIZATION': f'Bearer {self.token}'}
+        response: Response = self.client.put(
+            '/auth/update', data={
+                "password": "testpassword",
+                "email": new_email
+            }, format='json', **headers
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+        previous_di = self.user.details_iteration
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.email, new_email)
+        self.assertEqual(self.user.details_iteration, previous_di + 1)
+    
+    def test_update_password(self):
+        new_password = "new$assword"
+        headers = {'HTTP_AUTHORIZATION': f'Bearer {self.token}'}
+        response: Response = self.client.put(
+            '/auth/update', data={
+                "password": "testpassword",
+                "new_password": new_password
+            }, format='json', **headers
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+        previous_di = self.user.details_iteration
+        self.user.refresh_from_db()
+        self.assertTrue(security.check_password(self.user, new_password))
+        self.assertEqual(self.user.details_iteration, previous_di + 1)
+
+    def test_update_password_and_email(self):
+        new_email = "newemail@example.com"
+        new_password = "new$assword"
+        headers = {'HTTP_AUTHORIZATION': f'Bearer {self.token}'}
+        response: Response = self.client.put(
+            '/auth/update', data={
+                "password": "testpassword",
+                "email": new_email,
+                "new_password": new_password
+            }, format='json', **headers
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+        previous_di = self.user.details_iteration
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.email, new_email)
+        self.assertTrue(security.check_password(self.user, new_password))
+        self.assertEqual(self.user.details_iteration, previous_di + 1)
+
+    def test_invalid_password(self):
+        headers = {'HTTP_AUTHORIZATION': f'Bearer {self.token}'}
+        response: Response = self.client.put(
+            '/auth/update', data={
+                "password": "wrongpassword",
+                "email": "newemail@example.com"
+            }, format='json', **headers
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        previous_di = self.user.details_iteration
+        previous_email = self.user.email
+        self.user.refresh_from_db()
+        expected_data = {'detail': {'password': ['invalid password.']}}
+        self.assertEqual(response.data, expected_data)
+        self.assertEqual(self.user.email, previous_email)
+        self.assertEqual(self.user.details_iteration, previous_di)
+
+    def test_nothing_to_change(self):
+        headers = {'HTTP_AUTHORIZATION': f'Bearer {self.token}'}
+        response: Response = self.client.put(
+            '/auth/update', 
+            data={"password": "testpassword"}, 
+            format='json', **headers
+        )
+        previous_di = self.user.details_iteration
+        self.user.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        expected_data = {'detail': {'non_field_errors': ['nothing to change.']}}
+        self.assertEqual(response.data, expected_data)
+        self.assertEqual(self.user.details_iteration, previous_di)

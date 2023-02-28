1bd81b2574501af26810f20ea0de60a424f16758
rezvoj <rezac.vojtech.0@gmail.com>
2023-02-28 23:34:41 +0100

    recipe filtration tests

--- recipeAPIapp/tests/test_filtration.py
+++ recipeAPIapp/tests/test_filtration.py
@@ -721,3 +721,305 @@ class TestRatingFilter(APITestCase):
         self.assertIsNone(response.data['results'][0]['liked'])
         self.assertIn('user', response.data['results'][0])
         self.assertIn('recipe', response.data['results'][0])
+
+
+
+@override_settings(DEFAULT_FILE_STORAGE=media_utils.TEST_DEFAULT_FILE_STORAGE)
+@override_settings(MEDIA_ROOT=media_utils.TEST_MEDIA_ROOT)
+@override_settings(APP_ADMIN_CODE='TEST_ADMIN_CODE')
+class TestRecipeFilter(APITestCase):
+
+    def setUp(self):
+        self.test_user = User.objects.create(email=f"test_user@example.com", name=f"Test User")
+        self.user_token = security.generate_token(self.test_user)
+        self.moderator_user = User.objects.create(email=f"moderator@example.com", name=f"Moderator User", moderator=True)
+        self.moderator_token = security.generate_token(self.moderator_user)
+        """ 6 Categories """
+        self.category1 = Category.objects.create(name=f"Category 1", photo=media_utils.generate_test_image())
+        self.category2 = Category.objects.create(name=f"Category 2", photo=media_utils.generate_test_image())
+        self.category3 = Category.objects.create(name=f"Category 3", photo=media_utils.generate_test_image())
+        self.category4 = Category.objects.create(name=f"Category 4", photo=media_utils.generate_test_image())
+        self.category5 = Category.objects.create(name=f"Category 5", photo=media_utils.generate_test_image())
+        self.category6 = Category.objects.create(name=f"Category 6", photo=media_utils.generate_test_image())
+        """ Favoured categories """
+        self.category1.favoured_by.add(self.test_user)
+        self.category3.favoured_by.add(self.test_user)
+        self.category6.favoured_by.add(self.test_user)
+        """ 6 Ingredients """
+        self.ingredient1 = Ingredient.objects.create(
+            name=f"Ingredient 1", unit="g", photo=media_utils.generate_test_image()
+        )
+        self.ingredient2 = Ingredient.objects.create(
+            name=f"Ingredient 2", unit="kg", photo=media_utils.generate_test_image()
+        )
+        self.ingredient3 = Ingredient.objects.create(
+            name=f"Ingredient 3", unit="mg", photo=media_utils.generate_test_image()
+        )
+        self.ingredient4 = Ingredient.objects.create(
+            name=f"Ingredient 4", unit="mu", photo=media_utils.generate_test_image()
+        )
+        self.ingredient5 = Ingredient.objects.create(
+            name=f"Ingredient 5", unit="kg", photo=media_utils.generate_test_image()
+        )
+        self.ingredient6 = Ingredient.objects.create(
+            name=f"Ingredient 6", unit="g", photo=media_utils.generate_test_image()
+        )
+        """ Inventory ingredients """
+        UserIngredient.objects.create(user=self.test_user, ingredient=self.ingredient1, amount=300)
+        UserIngredient.objects.create(user=self.test_user, ingredient=self.ingredient2, amount=250)
+        UserIngredient.objects.create(user=self.test_user, ingredient=self.ingredient3, amount=400)
+        UserIngredient.objects.create(user=self.test_user, ingredient=self.ingredient4, amount=175)
+        UserIngredient.objects.create(user=self.test_user, ingredient=self.ingredient5, amount=250)
+        """ 3 Extra users """
+        self.user1 = User.objects.create(email=f"user1@example.com", name=f"User 1")
+        self.user2 = User.objects.create(email=f"user2@example.com", name=f"User 2")
+        self.user3 = User.objects.create(email=f"user3@example.com", name=f"User 3")
+
+        """ Recipe 1 (Great) by test_user: 2 ratings (1 new) 4.5 (4.0 new), 
+            3 categories (1, 2, 3), 2 ingredients (1, 4), favoured, photo (2), 6 servings """
+        self.recipe1 = Recipe.objects.create(
+            user=self.test_user, name="Recipe 1", title="Great Recipe 1",
+            submit_status=SubmitStatuses.ACCEPTED, 
+            prep_time=110, calories=290,
+            created_at=utc_now() - timedelta(days=2)
+        )
+        self.recipe1.favoured_by.add(self.test_user)
+        self.recipe1.categories.add(self.category1, self.category2, self.category3)
+        RecipePhoto.objects.create(recipe=self.recipe1, photo=media_utils.generate_test_image(), number=1)
+        RecipePhoto.objects.create(recipe=self.recipe1, photo=media_utils.generate_test_image(), number=2)
+        RecipeIngredient.objects.create(recipe=self.recipe1, ingredient=self.ingredient1, amount=50)
+        RecipeIngredient.objects.create(recipe=self.recipe1, ingredient=self.ingredient4, amount=20)
+        Rating.objects.create(user=self.user2, recipe=self.recipe1, stars=4)
+        Rating.objects.create(user=self.user3, recipe=self.recipe1, stars=5, created_at=utc_now() - timedelta(days=10))
+
+        """ Recipe 2 (Delicious) by test_user: 0 ratings, 
+            3 categories (2, 6), 2 ingredients (5, 2), DENIED """
+        self.recipe2 = Recipe.objects.create(
+            user=self.test_user, name="Recipe 2", title="Delicious Recipe 2", 
+            submit_status=SubmitStatuses.DENIED, deny_message = "This Recipe is denied",
+            prep_time=30, calories=250,
+            created_at=utc_now() - timedelta(days=1)
+        )
+        self.recipe2.categories.add(self.category2, self.category6)
+        RecipeIngredient.objects.create(recipe=self.recipe2, ingredient=self.ingredient5, amount=200)
+        RecipeIngredient.objects.create(recipe=self.recipe2, ingredient=self.ingredient2, amount=100)
+
+        """ Recipe 3 (Great) by user1: 3 ratings (0 new) 3.0, 
+            1 categories (4), 2 ingredients (1, 4), favoured, 3 servings"""
+        self.recipe3 = Recipe.objects.create(
+            user=self.user1, name="Recipe 3", title="Great Recipe 3", 
+            submit_status=SubmitStatuses.ACCEPTED, 
+            prep_time=60, calories=400,
+            created_at=utc_now() - timedelta(days=10)
+        )
+        self.recipe3.categories.add(self.category4)
+        RecipeIngredient.objects.create(recipe=self.recipe3, ingredient=self.ingredient1, amount=100)
+        RecipeIngredient.objects.create(recipe=self.recipe3, ingredient=self.ingredient4, amount=50)
+        Rating.objects.create(user=self.user1, recipe=self.recipe3, stars=3, created_at=utc_now() - timedelta(days=10))
+        Rating.objects.create(user=self.user3, recipe=self.recipe3, stars=2, created_at=utc_now() - timedelta(days=9))
+        Rating.objects.create(user=self.user2, recipe=self.recipe3, stars=4, created_at=utc_now() - timedelta(days=8))
+        self.recipe3.favoured_by.add(self.test_user)
+
+        """ Recipe 4 (Delicious) by user1: 1 ratings (1 new) 1.0, 
+            1 categories (1, 6), 3 ingredients (1, 2, 5), 3 servings """
+        self.recipe4 = Recipe.objects.create(
+            user=self.user1, name="Recipe 4", title="Delicious Recipe 4", 
+            submit_status=SubmitStatuses.ACCEPTED, 
+            prep_time=20, calories=200,
+            created_at=utc_now() - timedelta(days=2)
+        )
+        self.recipe4.categories.add(self.category1, self.category6)
+        RecipeIngredient.objects.create(recipe=self.recipe4, ingredient=self.ingredient1, amount=50)
+        RecipeIngredient.objects.create(recipe=self.recipe4, ingredient=self.ingredient2, amount=70)
+        RecipeIngredient.objects.create(recipe=self.recipe4, ingredient=self.ingredient5, amount=10)
+        Rating.objects.create(user=self.user1, recipe=self.recipe4, stars=1)
+
+        """ Recipe 5 (Great) by user2: 0 ratings, 
+            1 categories (1), 1 ingredients (6), favoured, 0 servings """
+        self.recipe5 = Recipe.objects.create(
+            user=self.user2, name="Recipe 5", title="Great Recipe 5", 
+            submit_status=SubmitStatuses.ACCEPTED, 
+            prep_time=50, calories=350,
+            created_at=utc_now() - timedelta(days=5)
+        )
+        self.recipe5.categories.add(self.category1)
+        RecipeIngredient.objects.create(recipe=self.recipe5, ingredient=self.ingredient6, amount=70)
+        self.recipe5.favoured_by.add(self.test_user)
+
+        """ Recipe 6 (Great) by user3: 4 ratings (4 new) 2.5 (2.5 new),
+            2 categories (1, 3), 2 ingredients (3, 4), 1 servings """
+        self.recipe6 = Recipe.objects.create(
+            user=self.user3, name="Recipe 6", title="Great Recipe 6", 
+            submit_status=SubmitStatuses.ACCEPTED, 
+            prep_time=40, calories=280,
+            created_at=utc_now() - timedelta(days=7)
+        )
+        self.recipe6.categories.add(self.category1, self.category3)
+        RecipeIngredient.objects.create(recipe=self.recipe6, ingredient=self.ingredient3, amount=50)
+        RecipeIngredient.objects.create(recipe=self.recipe6, ingredient=self.ingredient4, amount=115)
+        Rating.objects.create(user=self.user1, recipe=self.recipe6, stars=2)
+        Rating.objects.create(user=self.user2, recipe=self.recipe6, stars=4)
+        Rating.objects.create(user=self.user3, recipe=self.recipe6, stars=1)
+        Rating.objects.create(user=self.test_user, recipe=self.recipe6, stars=3)
+
+        """ Recipe 7 (Delicious) by user3: 0 ratings,
+            2 categories (5), 2 ingredients (1, 2), SUBMITTED """
+        self.recipe7 = Recipe.objects.create(
+            user=self.user3, name="Recipe 7", title="Delicious Recipe 7", 
+            submit_status=SubmitStatuses.SUBMITTED,
+            prep_time=40, calories=280,
+            created_at=utc_now() - timedelta(days=17)
+        )
+        self.recipe7.categories.add(self.category5)
+        RecipeIngredient.objects.create(recipe=self.recipe7, ingredient=self.ingredient1, amount=115)
+        RecipeIngredient.objects.create(recipe=self.recipe7, ingredient=self.ingredient2, amount=200)
+
+
+    def tearDown(self):
+        media_utils.delete_test_media()
+
+
+    def test_data(self):
+        headers = {'HTTP_AUTHORIZATION': f"Bearer {self.user_token}"}
+        params = {
+            'user': self.test_user.pk, 'favourite_category': True,
+            'submit_status': SubmitStatuses.ACCEPTED, 
+            'page': 1, 'page_size': 5
+        }
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json', **headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 1)
+        expected_recipe = {
+            'id': self.recipe1.pk, 'name': 'Recipe 1', 'title': 'Great Recipe 1',
+            'photo': RecipePhoto.objects.filter(recipe=self.recipe1, number=1).first().photo.url,
+            'created_at': self.recipe1.created_at.isoformat(),
+            'deny_message': None, 'favoured': True,
+            'prep_time': 110, 'calories': 290,
+            'rating_count': 2, 'avg_rating': 4.5,
+            'submit_status': 'ACCEPTED',
+            'user': {
+                'created_at': self.test_user.created_at.isoformat(),
+                'id': self.test_user.pk, 
+                'name': 'Test User', 'photo': None
+                }
+            }
+        self.assertEqual(response.data['results'][0], expected_recipe)
+
+
+    def test_submit_status(self):
+        headers = {'HTTP_AUTHORIZATION': f"Bearer {self.user_token}"}
+        params = {'submit_status': SubmitStatuses.DENIED, 'page': 1, 'page_size': 5}
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json', **headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['detail'], {'submit_status': ['invalid submit_status value.']})
+        params['user'] = self.test_user.pk
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json', **headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 1)
+        self.assertEqual(response.data['results'][0]['id'], self.recipe2.pk)
+        self.assertEqual(response.data['results'][0]['deny_message'], "This Recipe is denied")
+        headers = {}
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json', **headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['detail'], {'submit_status': ['invalid submit_status value.']})
+        params['user'] = self.user3.pk
+        params['submit_status'] = SubmitStatuses.SUBMITTED
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json', **headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['detail'], {'submit_status': ['invalid submit_status value.']})        
+        params.pop('user')
+        headers = {'HTTP_AUTHORIZATION': f"Bearer {self.moderator_token}"}
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json', **headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 1)
+        self.assertEqual(response.data['results'][0]['id'], self.recipe7.pk)  
+        params['submit_status'] = SubmitStatuses.DENIED
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json', **headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['detail'], {'submit_status': ['invalid submit_status value.']})
+
+
+    def test_categories(self):
+        headers = {'HTTP_AUTHORIZATION': f"Bearer {self.user_token}"}
+        params = {
+            'favourite_category': True,
+            'prep_time_limit': 100, 
+            'calories_limit': 600, 'servings': 2,
+            'order_by': ['-created_at', '-name'],
+            'page': 1, 'page_size': 5
+        }
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json', **headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 2)
+        self.assertEqual(response.data['results'][0]['id'], self.recipe4.pk)
+        self.assertEqual(response.data['results'][1]['id'], self.recipe6.pk)
+        headers = {'HTTP_AUTHORIZATION': f"Bearer {self.user_token}"}
+        params = {
+            'categories': [self.category2.pk, self.category3.pk],
+            'order_by': ['created_at', 'name'],
+            'page': 1, 'page_size': 5
+        }
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json', **headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 2)
+        self.assertEqual(response.data['results'][0]['id'], self.recipe6.pk)
+        self.assertEqual(response.data['results'][1]['id'], self.recipe1.pk)
+
+
+    def test_sufficient_ingrediens(self):
+        headers = {'HTTP_AUTHORIZATION': f"Bearer {self.user_token}"}
+        params = {
+            'favoured': True, 'sufficient_ingrediens': True, 
+            'servings': 3, 'order_by': ['name'],
+            'page': 1, 'page_size': 5
+        }
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json', **headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 2)
+        self.assertEqual(response.data['results'][0]['id'], self.recipe1.pk)
+        self.assertEqual(response.data['results'][1]['id'], self.recipe3.pk)
+        params['servings'] = 6
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json', **headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 1)
+        self.assertEqual(response.data['results'][0]['id'], self.recipe1.pk)
+        params['servings'] = 7
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json', **headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 0)
+
+
+    def test_ordering(self):
+        params = {
+            'order_by': ['-rating_count', '-created_at', 'name'],
+            'order_time_window': 5, 'search_string': "great's", 
+            'page': 1, 'page_size': 5
+        }
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 4)
+        self.assertEqual(response.data['results'][0]['id'], self.recipe6.pk)
+        self.assertEqual(response.data['results'][0]['rating_count'], 4)
+        self.assertEqual(response.data['results'][1]['id'], self.recipe1.pk)
+        self.assertEqual(response.data['results'][1]['rating_count'], 2)
+        self.assertEqual(response.data['results'][2]['id'], self.recipe5.pk)
+        self.assertEqual(response.data['results'][2]['rating_count'], 0)
+        self.assertEqual(response.data['results'][3]['id'], self.recipe3.pk)
+        self.assertEqual(response.data['results'][3]['rating_count'], 3)
+        params = {
+            'order_by': ['-avg_rating', '-created_at', 'name'],
+            'order_time_window': 5, 'page': 1, 'page_size': 5
+        }
+        response: Response = self.client.get(f'/recipe/filter/paged', params, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 5)
+        self.assertEqual(response.data['results'][0]['id'], self.recipe1.pk)
+        self.assertEqual(response.data['results'][0]['avg_rating'], 4.5)
+        self.assertEqual(response.data['results'][1]['id'], self.recipe6.pk)
+        self.assertEqual(response.data['results'][1]['avg_rating'], 2.5)
+        self.assertEqual(response.data['results'][2]['id'], self.recipe4.pk)
+        self.assertEqual(response.data['results'][2]['avg_rating'], 1.0)
+        self.assertEqual(response.data['results'][3]['id'], self.recipe5.pk)
+        self.assertEqual(response.data['results'][3]['avg_rating'], None)
+        self.assertEqual(response.data['results'][4]['id'], self.recipe3.pk)
+        self.assertEqual(response.data['results'][4]['avg_rating'], 3.0)
